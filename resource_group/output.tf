bce7dbce48833357dafd6346bcb1420b31164ef1
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2025-02-10 16:51:38 -0300

    Add files via upload

---
+++ resource_group/output.tf
@@ -0,0 +1,7 @@
+output "rg_name_marcelo" {
+    value = azurerm_resource_group.rg-marcelo.name  
+}
+
+output "location" {
+  value = azurerm_resource_group.rg-marcelo.location
+}
---
+++ resource_group/variable.tf
@@ -0,0 +1,9 @@
+variable "rg_name" {
+    description = "Nome Resource Group (Marcelo)"
+    type = string
+}
+
+variable "rg_location" {
+    description = "Localização RG"
+    type = string
+}
